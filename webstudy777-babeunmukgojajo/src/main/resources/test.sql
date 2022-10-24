15979d11fc14ec9a278f68173618948d129eea61
yerin-ddadda <yerin0110@naver.com>
2022-10-24 17:49:17 +0900

    join 답

--- webstudy777-babeunmukgojajo/src/main/resources/test.sql
+++ webstudy777-babeunmukgojajo/src/main/resources/test.sql
@@ -7,9 +7,26 @@ delete from board
 INSERT INTO BOARD(no, title, content, time_posted, id) VALUES(board_seq.nextval, '공부', '월요일 즐겁네요', sysdate, 'java');
 INSERT INTO BOARD(no, title, content, time_posted, id) VALUES(board_seq.nextval, '공부2', '월요일 기쁘네요', sysdate, 'spring');
 INSERT INTO BOARD(no, title, content, time_posted, id) VALUES(board_seq.nextval, '공부3', '월요일 행복해요', sysdate, 'java');
-INSERT INTO BOARD(no, title, content, time_posted, id) VALUES(board_seq.nextval, '제목', '내용1', sysdate, 'python');
-INSERT INTO BOARD(no, title, content, time_posted, id) VALUES(board_seq.nextval, '제목입니다', '내용2', sysdate, 'mybatis');
+/*INSERT INTO BOARD(no, title, content, time_posted, id) VALUES(board_seq.nextval, '제목', '내용1', sysdate, 'python');
+INSERT INTO BOARD(no, title, content, time_posted, id) VALUES(board_seq.nextval, '제목입니다', '내용2', sysdate, 'mybatis');*/
 INSERT INTO BOARD(no, title, content, time_posted, id) VALUES(board_seq.nextval, '공부3', '내용3', sysdate, 'spring');
 
 select * from board
 
+
+/*oracle join*/
+SELECT b.no, b.title, b.content, b.time_posted, b.id
+FROM board b JOIN community_member cm
+ON b.id=cm.id
+where b.title in('공부', '제목');
+
+/*ANSI SQL*/
+SELECT b.no, b.title, b.content, b.time_posted, b.id
+FROM board b
+INNER JOIN	community_member cm ON b.id = cm.id --결합할 테이블을 JOIN 뒤에
+where b.title ='공부' or b.title= '제목';
+
+SELECT b.no, b.title, b.content, b.time_posted, b.id
+FROM board b
+INNER JOIN	community_member cm ON b.id = cm.id --결합할 테이블을 JOIN 뒤에
+where b.title in('공부','제목');
