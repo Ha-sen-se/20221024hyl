5071f557afa3785b95c8e2c78a5da57f086316a4
yerin-ddadda <yerin0110@naver.com>
2022-10-24 17:30:30 +0900

    commit-join study

--- webstudy777-babeunmukgojajo/src/main/resources/test.sql
+++ webstudy777-babeunmukgojajo/src/main/resources/test.sql
@@ -1,10 +1,15 @@
 /*
  * BOARD 테이블과 community_member을 이용해서 
- * 단, 직업(job)이 CLERK인 사원이거나 Manager인 사원만 조회 제목이 '공부'이거나 '제목'인  게시물 조회
+ 	제목이 '공부'이거나 '제목'인  게시물 조회
  */
+delete from board
 
+INSERT INTO BOARD(no, title, content, time_posted, id) VALUES(board_seq.nextval, '공부', '월요일 즐겁네요', sysdate, 'java');
+INSERT INTO BOARD(no, title, content, time_posted, id) VALUES(board_seq.nextval, '공부2', '월요일 기쁘네요', sysdate, 'spring');
+INSERT INTO BOARD(no, title, content, time_posted, id) VALUES(board_seq.nextval, '공부3', '월요일 행복해요', sysdate, 'java');
 INSERT INTO BOARD(no, title, content, time_posted, id) VALUES(board_seq.nextval, '제목', '내용1', sysdate, 'python');
 INSERT INTO BOARD(no, title, content, time_posted, id) VALUES(board_seq.nextval, '제목입니다', '내용2', sysdate, 'mybatis');
 INSERT INTO BOARD(no, title, content, time_posted, id) VALUES(board_seq.nextval, '공부3', '내용3', sysdate, 'spring');
 
+select * from board
 
