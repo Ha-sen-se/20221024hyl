396cc325fe477e05ed8aa26e14dc7ef1dcccb17c
kimyunsoo56 <kimyunsoo56@gmail.com>
2022-10-24 17:48:14 +0900

    윤수

--- webstudy777-babeunmukgojajo/src/main/resources/test.sql
+++ webstudy777-babeunmukgojajo/src/main/resources/test.sql
@@ -7,9 +7,16 @@ delete from board
 INSERT INTO BOARD(no, title, content, time_posted, id) VALUES(board_seq.nextval, '공부', '월요일 즐겁네요', sysdate, 'java');
 INSERT INTO BOARD(no, title, content, time_posted, id) VALUES(board_seq.nextval, '공부2', '월요일 기쁘네요', sysdate, 'spring');
 INSERT INTO BOARD(no, title, content, time_posted, id) VALUES(board_seq.nextval, '공부3', '월요일 행복해요', sysdate, 'java');
-INSERT INTO BOARD(no, title, content, time_posted, id) VALUES(board_seq.nextval, '제목', '내용1', sysdate, 'python');
-INSERT INTO BOARD(no, title, content, time_posted, id) VALUES(board_seq.nextval, '제목입니다', '내용2', sysdate, 'mybatis');
-INSERT INTO BOARD(no, title, content, time_posted, id) VALUES(board_seq.nextval, '공부3', '내용3', sysdate, 'spring');
+
+INSERT INTO BOARD(no, title, content, time_posted, id) VALUES(board_seq.nextval, '제목', '내용3', sysdate, 'spring');
 
 select * from board
+select * from community_member
+
+SELECT b.no,b.title,b.content,b.time_posted,c.id 
+FROM board b
+INNER JOIN community_member c ON b.id=c.id
+WHERE b.title='공부' OR b.title='제목';
+
+
 
